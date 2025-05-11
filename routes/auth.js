d0b85bdb84b0da6205d22ab1b8129f8247b06847
Afolabi Ogunbanwo <afolabiogunbanwo@gmail.com>
2025-05-11 01:26:04 +0100

    added production url

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,14 @@ const router = express.Router();
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:8000/auth/google/callback'
+  'https://hai-api.onrender.com/google/callback'
 );
+
+// const oauth2Client = new google.auth.OAuth2(
+//   process.env.GOOGLE_CLIENT_ID,
+//   process.env.GOOGLE_CLIENT_SECRET,
+//   'http://localhost:8000/auth/google/callback'
+// );
 
 // Scopes for Gmail access
 const SCOPES = [
