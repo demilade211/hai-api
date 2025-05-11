94f17a18d0002579eabdc9809ef25c9e13902dd9
Afolabi Ogunbanwo <afolabiogunbanwo@gmail.com>
2025-05-12 00:18:46 +0100

    put secure true for prod only

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv"
+
+dotenv.config({ path: "../config/config.env" }); 
 // Create and send token and save in the cookie.
 const sendToken = (user, statusCode, res, token) => {
 
@@ -8,9 +11,12 @@ const sendToken = (user, statusCode, res, token) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRY_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        secure: true, // important for cookies to work over HTTPS (e.g., on Vercel)
+        httpOnly: true, 
         sameSite: 'None', // for cross-origin cookies (if backend & frontend are on different domains)
+    }
+
+    if(process.env.NODE_ENV === 'PRODUCTION'){
+        options.secure = true; // Set to true for production important for cookies to work over HTTPS (e.g., on Vercel)
     }
 
 
